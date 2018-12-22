b8e2a4f08e948637824729df68ee6669ef4ccd27
Pirmin Kalberer <pka@sourcepole.ch>
2018-12-22 17:03:25 +0100

    Add GeoJSON layer

--- storymap/index.js
+++ storymap/index.js
@@ -1,6 +1,7 @@
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import './index.css';
+import etappen from './etappen.json';
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoic291cmNlcG9sZSIsImEiOiJjanB6bWxoazEwY2plNDhsY3RpenBvNmM0In0.Z--2Th6QmaLc_OhPDNsiwg';
 const map = new mapboxgl.Map({
@@ -8,4 +9,15 @@ const map = new mapboxgl.Map({
     style: 'mapbox://styles/mapbox/streets-v9',
     center: [8.228760,46.873336],
     zoom: 7
+});
+
+map.on('load', function () {
+    map.addLayer({
+        'id': 'etappen',
+        'type': 'line',
+        'source': {
+            'type': 'geojson',
+            'data': etappen
+        }
+    });
 });
